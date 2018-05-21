64c3a3b35ac3b78b3756fa58b4a77dd8feec328e
Ricardo Aoun <contato@aoun.com.br>
2018-05-21 15:10:05 -0300

    Salvando os dados do usuário logado no localStorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { StorageService } from './../services/storage.service';
 import { CategoriaService } from './../services/domain/categoria.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -28,7 +29,8 @@ import { AuthService } from '../services/auth.service';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoriaService,
-    AuthService
+    AuthService,
+    StorageService
   ]
 })
 export class AppModule {}
