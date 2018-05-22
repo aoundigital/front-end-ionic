dbdbe1125e918ae1b6eb2a120e9b8ed4794e350c
Ricardo Aoun <contato@aoun.com.br>
2018-05-22 13:40:06 -0300

    Salvando novo cliente

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -20,4 +20,15 @@ export class ClienteService {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
         return this.http.get(url, {responseType : 'blob'});
     }
+
+    insert(obj : ClienteDTO){ //insert recebento obj do tipo CileiteDTO
+        return this.http.post( // fazer um POST
+            `${API_CONFIG.baseUrl}/clientes`, // endpoint Clientes
+            obj, // passar o obj
+            {
+                observe: 'response',
+                responseType: 'text' // esperar uma resposta do tipo texto.
+            }
+        );
+    }
 }
