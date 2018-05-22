10d5d9d4da78f0c4294f9d9b4f8f09002fbe5a34
Ricardo Aoun <contato@aoun.com.br>
2018-05-22 15:32:24 -0300

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { subscribeOn } from 'rxjs/operator/subscribeOn';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -20,9 +21,21 @@ export class ProdutosPage {
     let categoria_id = this.navParams.get('categoria_id'); // pega o parâmetro enviado da pagina de categorias.
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response =>{
-        this.items = response['content'];//pega na resposta só o atriboto content. 
+        this.items = response['content'];//pega na resposta só o atriboto content.
+        this.loadImageUrls(); // chama o método para trazer a imagem de cada produto.
       },
     error => {});
+  }
+
+  loadImageUrls(){
+    for(var i=0; i<this.items.length; i++){ //percorrer a lista de produtos
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id) //pega o id do produto
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;//monta a URL da imagem do produto.
+        },
+      error => {});
+    }
   }
 
 }
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -12,4 +12,9 @@ export class ProdutoService {
     findByCategoria(categoria_id : string) { //recebe o ID da Categoria
         return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);//monta s URL para o GET 
     }
+
+    getSmallImageFromBucket(id : string) : Observable<any>{ 
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg` // cria a url das imagens com ID do produto.
+        return this.http.get(url, {responseType : 'blob'});// GET na url da imagem.
+    }
 }
